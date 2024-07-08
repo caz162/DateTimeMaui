96a20621de38c6dc79c1ee02c87acf774cd9f983
callum terris <callum.terris@clevermed.com>
2024-07-08 16:17:19 +0100

    Example app

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace DateTimeExample;
+
+public partial class MainPage : ContentPage
+{
+	public string DateTimeMin { get; } = $"Non UTC: {DateTime.MinValue.ToString()}";
+	public string DateTimeMinUTC { get; } = $"Non UTC: {DateTime.MinValue.ToUniversalTime().ToString()}";
+
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+}
+
